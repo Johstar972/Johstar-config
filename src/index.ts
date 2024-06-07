16147670f2ea65c0821ee61716177913a6a41766
Johstar <pezojohan@gmail.com>
2024-06-07 14:15:19 +0200

    test husky

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+const sum = (a: number, b: number): number => {
+    return a + b;
+}
+
+const substract = (a: number, b: number): number => {
+    return a - b;
+}
+
+const multiply = (a: number, b: number): number => {
+    return a * b;
+}
+
+const divide = (a: number, b: number): number => {
+    return a / b;
+}
+
+const displayResult = (a: number) => {
+    console.log(`${a}`);
+}
+
