45f9508637949c0051d8b0f0cf91646a1503cce1
Johstar <pezojohan@gmail.com>
2024-06-07 15:49:42 +0200

    chore: add new prettier and eslint config

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
 const sum = (a: number, b: number): number => {
-    return a + b;
-}
+  return a + b;
+};
 
 const substract = (a: number, b: number): number => {
-    return a - b;
-}
+  return a - b;
+};
 
 const multiply = (a: number, b: number): number => {
-    return a * b;
-}
+  return a * b;
+};
 
 const divide = (a: number, b: number): number => {
-    return a / b;
-}
+  return a / b;
+};
 
 const displayResult = (a: number) => {
-    console.log(`${a}`);
-}
-
+  console.log(`${a}`);
+};
